f0c8e78028c6ae037fb8e05fa5bf7ed0971b4b0d
AlexLab <41185842+AlexDeran@users.noreply.github.com>
2021-02-14 14:25:21 +0100

    Update sbp2.sql

--- sql/sbp2.sql
+++ sql/sbp2.sql
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données : `id15602210_sbp`
+-- Base de données : `sbp`
 --
 
 -- --------------------------------------------------------
